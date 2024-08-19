1078b9e37029989dfb1f1d2d5fb9a8699d8ac76c
kimu805 <kimukimuok805@gmail.com>
2024-08-19 19:03:31 +0900

    define association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   has_many :tweets
+
+  # フォローする側のUserから見た中間テーブル
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  # フォローされる側のUserから見た中間テーブル
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
 end
